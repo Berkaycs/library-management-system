bf6002c1306b3c21de509d2c8ed873949a46fd00
berkaypicakci <berkaypicakci@outlook.com>
2024-01-26 21:20:17 +0300

    third commit
    - creating book SO
    - creating add book panel UI
    - creating a script to manage the book list and instantiate the books

--- Assets/Scripts/AddBookPanelController.cs
+++ Assets/Scripts/AddBookPanelController.cs
@@ -1,16 +1,36 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class AddBookPanelController : MonoBehaviour
 {
-    [SerializeField] private InputField isbnInput;
-    [SerializeField] private InputField titleInput;
-    [SerializeField] private InputField authorInput;
-    [SerializeField] private InputField copyNumberInput;
+    [SerializeField] private TMP_InputField isbnInput;
+    [SerializeField] private TMP_InputField titleInput;
+    [SerializeField] private TMP_InputField authorInput;
+    [SerializeField] private TMP_InputField copyNumberInput;
 
-    [SerializeField] private BookSO bookSO;
+    [SerializeField] private BookManager bookManager;
+
+    [SerializeField] private List<GameObject> books;
+
+    private float x = 2.2f;
+    private float y = 2.2f;
+    private float z = 0.8f;
+
+    private void Awake()
+    {
+        foreach (BookInfo bookSO in bookManager.books)
+        {
+            for (int i = 0; i < bookSO.copyNumber - bookSO.borrowedNumber; i++)
+            {
+                Vector3 pos = new Vector3(x, y, z);
+                Quaternion rot = Quaternion.Euler(0f, 270f, 0f);
+                Instantiate(bookSO.bookPrefab, pos, rot);
+                BookPos();
+            }
+        }
+    }
 
     public void OnAddButtonClick()
     {
@@ -18,16 +38,30 @@ public class AddBookPanelController : MonoBehaviour
 
         if (existingBook != null )
         {
-            existingBook.copyNumber++;
+            existingBook.copyNumber += int.Parse(copyNumberInput.text);
+
+            for (int i = 0; i < int.Parse(copyNumberInput.text); i++)
+            {
+                Vector3 pos = new Vector3(x, y, z);
+                Quaternion rot = Quaternion.Euler(0f, 270f, 0f);
+                Instantiate(existingBook.bookPrefab, pos, rot);
+                BookPos();
+            }
         }
 
         else
         {
             BookInfo newBook = CreateBook();
-
             AddBookToBookSO(newBook);
-        }
 
+            for (int i = 0; i < newBook.copyNumber; i++)
+            {
+                Vector3 pos = new Vector3(x, y, z);
+                Quaternion rot = Quaternion.Euler(0f, 270f, 0f);
+                Instantiate(newBook.bookPrefab, pos, rot);
+                BookPos();
+            }
+        }
         // Optionally, close the UI panel or reset input fields
         // panel.SetActive(false);
         // isbnInput.text = "";
@@ -38,9 +72,9 @@ public class AddBookPanelController : MonoBehaviour
 
     private BookInfo FindExistingBook(string isbn)
     {
-        if (bookSO != null && bookSO.books != null)
+        if (bookManager != null && bookManager.books != null)
         {
-            foreach (BookInfo book in bookSO.books)
+            foreach (BookInfo book in bookManager.books)
             {
                 if (book.isbn == isbn)
                 {
@@ -54,21 +88,48 @@ public class AddBookPanelController : MonoBehaviour
 
     private BookInfo CreateBook()
     {
+        int bookIndex = Random.Range(0, books.Count);
         BookInfo book = ScriptableObject.CreateInstance<BookInfo>();
+        book.bookPrefab = books[bookIndex];
         book.isbn = isbnInput.text;
         book.title = titleInput.text;
         book.author = authorInput.text;
         book.copyNumber = int.Parse(copyNumberInput.text);
+        book.borrowedNumber = 0;
+
+        string filePath = $"Assets/Resources/{book.title}.asset";
+
+        UnityEditor.AssetDatabase.CreateAsset(book, filePath);
+        UnityEditor.AssetDatabase.SaveAssets();
+        UnityEditor.AssetDatabase.Refresh();
 
         return book;
     }
 
     private void AddBookToBookSO(BookInfo book)
     {
-        if (bookSO != null && bookSO.books != null)
+        if (bookManager != null && bookManager.books != null)
         {
-            System.Array.Resize(ref bookSO.books, bookSO.books.Length + 1);
-            bookSO.books[bookSO.books.Length - 1] = book;
+            bookManager.books.Add(book);
+        }
+    }
+
+    private void BookPos()
+    {
+        if (z <= 0.8f && z > -0.8f)
+        {
+            z -= 0.055f;
+        }
+
+        else if (y <= 2.2f && y >= 0.6f)
+        {
+            y -= 0.4f;
+            z = 0.8f;
+        }
+
+        else
+        {
+            Debug.Log("The bookshelf is full!");
         }
     }
 }
--- Assets/Scripts/BookInfo.cs
+++ Assets/Scripts/BookInfo.cs
@@ -5,20 +5,10 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class BookInfo : ScriptableObject
 {
-    public BookType bookType;
-
     public GameObject bookPrefab;
     public string isbn;
     public string title;
     public string author;
     public int copyNumber;
     public int borrowedNumber;
-
-    public enum BookType
-    {
-        Adventure,
-        Mystery,
-        Horror,
-        Science
-    }
 }
